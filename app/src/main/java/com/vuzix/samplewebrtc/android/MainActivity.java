42358bd7913328173da1119b6b993daa324ff6c6
Keiji Ariyama <keiji_ariyama@c-lis.co.jp>
2016-02-26 01:44:42 +0900

    動作確認バージョン動いた。
    シグナリングサーバーはopenwebrtc.orgを使用。

--- app/src/main/java/com/vuzix/samplewebrtc/android/MainActivity.java
+++ app/src/main/java/com/vuzix/samplewebrtc/android/MainActivity.java
@@ -2,74 +2,247 @@ package com.vuzix.samplewebrtc.android;
 
 import android.app.Activity;
 import android.content.pm.ActivityInfo;
-import android.graphics.SurfaceTexture;
 import android.os.Bundle;
-import android.view.Surface;
+import android.util.Log;
 import android.view.TextureView;
+import android.view.View;
 import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
-import com.ericsson.research.owr.CaptureSourcesCallback;
-import com.ericsson.research.owr.MediaSource;
-import com.ericsson.research.owr.MediaType;
 import com.ericsson.research.owr.Owr;
-import com.ericsson.research.owr.VideoRenderer;
-import com.ericsson.research.owr.WindowRegistry;
+import com.ericsson.research.owr.sdk.InvalidDescriptionException;
+import com.ericsson.research.owr.sdk.RtcCandidate;
+import com.ericsson.research.owr.sdk.RtcCandidates;
+import com.ericsson.research.owr.sdk.RtcConfig;
+import com.ericsson.research.owr.sdk.RtcConfigs;
+import com.ericsson.research.owr.sdk.RtcSession;
+import com.ericsson.research.owr.sdk.RtcSessions;
+import com.ericsson.research.owr.sdk.SessionDescription;
+import com.ericsson.research.owr.sdk.SessionDescriptions;
+import com.ericsson.research.owr.sdk.SimpleStreamSet;
+import com.ericsson.research.owr.sdk.VideoView;
 
-import java.util.EnumSet;
-import java.util.List;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends Activity {
     private static final String TAG = MainActivity.class.getSimpleName();
+    public static final String SDP = "sdp";
+
+    private final RtcConfig RTC_CONFIG = RtcConfigs.defaultConfig(BuildConfig.ICE_SERVER);
 
     static {
         Owr.init();
         Owr.runInBackground();
     }
 
-    private TextureView mTextureView;
-    private final TextureView.SurfaceTextureListener mSurfaceTextureListener
-            = new TextureView.SurfaceTextureListener() {
-        @Override
-        public void onSurfaceTextureAvailable(final SurfaceTexture surfaceTexture, final int width, final int height) {
-            Surface surface = new Surface(surfaceTexture);
-            WindowRegistry.get().register(TAG, surface);
-        }
+    @Bind(R.id.session_id)
+    TextView mSessionId;
 
-        @Override
-        public void onSurfaceTextureSizeChanged(final SurfaceTexture surface, final int width, final int height) {
-        }
+    @Bind(R.id.texture_view)
+    TextureView mVideoView;
 
-        @Override
-        public boolean onSurfaceTextureDestroyed(final SurfaceTexture surface) {
-            WindowRegistry.get().unregister(TAG);
-            return true;
-        }
+    private SessionChannel mSessionChannel;
 
+    private final RtcSession.OnLocalCandidateListener mOnLocalCandidateListener
+            = new RtcSession.OnLocalCandidateListener() {
         @Override
-        public void onSurfaceTextureUpdated(final SurfaceTexture surface) {
+        public void onLocalCandidate(RtcCandidate candidate) {
+            Log.d(TAG, "onLocalCandidate");
+            if (mPeerChannel == null) {
+                return;
+            }
+
+            Log.d(TAG, "onLocalCandidate2");
+
+            try {
+                JSONObject json = new JSONObject();
+                json.putOpt("candidate", RtcCandidates.toJsep(candidate));
+                json.getJSONObject("candidate").put("sdpMid", "video");
+                mSessionChannel.send(mPeerChannel, json);
+            } catch (JSONException e) {
+                Log.e(TAG, "JSONException", e);
+            }
         }
     };
+
+    private SessionChannel.Peer mPeerChannel;
+
+    private final RtcSession.OnLocalDescriptionListener mOnLocalDescriptionListener
+            = new RtcSession.OnLocalDescriptionListener() {
+        @Override
+        public void onLocalDescription(SessionDescription localDescription) {
+            Log.d(TAG, "onLocalDescription");
+
+            if (mPeerChannel == null) {
+                return;
+            }
+
+            try {
+                JSONObject json = new JSONObject();
+                json.putOpt(SDP, SessionDescriptions.toJsep(localDescription));
+                Log.d(TAG, "sending sdp: " + json);
+                mSessionChannel.send(mPeerChannel, json);
+            } catch (JSONException e) {
+                Log.d(TAG, "JSONException", e);
+            }
+        }
+    };
+
+    private final SessionChannel.PeerDisconnectListener mOnDisconnectedListener
+            = new SessionChannel.PeerDisconnectListener() {
+        @Override
+        public void onPeerDisconnect(SessionChannel.Peer peer) {
+            Log.d(TAG, "onPeerDisconnect");
+
+            mRtcSession.stop();
+            mPeerChannel = null;
+        }
+    };
+
+    private final SessionChannel.MessageListener mOnMessageListener
+            = new SessionChannel.MessageListener() {
+        @Override
+        public void onMessage(JSONObject data) {
+            Log.d(TAG, "onMessage " + data);
+
+            if (data.has("candidate")) {
+                JSONObject candidate = data.optJSONObject("candidate");
+                Log.v(TAG, "candidate: " + candidate);
+                RtcCandidate rtcCandidate = RtcCandidates.fromJsep(candidate);
+                if (rtcCandidate != null) {
+                    mRtcSession.addRemoteCandidate(rtcCandidate);
+                }
+            }
+            if (data.has(SDP)) {
+                JSONObject sdp = data.optJSONObject(SDP);
+                try {
+                    SessionDescription sessionDescription = SessionDescriptions.fromJsep(sdp);
+                    if (sessionDescription.getType() == SessionDescription.Type.OFFER) {
+                        onInboundCall(sessionDescription);
+                    } else {
+                        onAnswer(sessionDescription);
+                    }
+                } catch (InvalidDescriptionException e) {
+                    Log.d(TAG, "InvalidDescriptionException", e);
+                }
+            }
+        }
+    };
+
+    private void onAnswer(SessionDescription sessionDescription) {
+        Log.d(TAG, "onAnswer");
+
+        if (mRtcSession == null) {
+            return;
+        }
+
+        try {
+            mRtcSession.setRemoteDescription(sessionDescription);
+        } catch (InvalidDescriptionException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void onInboundCall(SessionDescription sessionDescription) {
+        Log.d(TAG, "onInboundCall");
+
+        try {
+            mRtcSession.setRemoteDescription(sessionDescription);
+            mRtcSession.start(mStreamSet);
+        } catch (InvalidDescriptionException e) {
+        }
+    }
+
+    private final SessionChannel.JoinPeerListener mJoinListener
+            = new SessionChannel.JoinPeerListener() {
+        @Override
+        public void onPeerJoin(SessionChannel.Peer peer) {
+            Log.d(TAG, "onPeerJoin");
+
+            mPeerChannel = peer;
+            mPeerChannel.setDisconnectListener(mOnDisconnectedListener);
+            mPeerChannel.setMessageListener(mOnMessageListener);
+
+            mRtcSession = RtcSessions.create(RTC_CONFIG);
+            mRtcSession.setOnLocalCandidateListener(mOnLocalCandidateListener);
+            mRtcSession.setOnLocalDescriptionListener(mOnLocalDescriptionListener);
+        }
+    };
+
+    private final SessionChannel.DisconnectListener mDisconnectListener
+            = new SessionChannel.DisconnectListener() {
+        @Override
+        public void onDisconnect() {
+            Log.d(TAG, "onDisconnect");
+
+            Toast.makeText(MainActivity.this, "Disconnected from server", Toast.LENGTH_LONG).show();
+
+            mRtcSession.stop();
+            mStreamSet = null;
+            mRtcSession = null;
+            mSessionChannel = null;
+        }
+    };
+
+    private final SessionChannel.SessionFullListener mSessionFullListener
+            = new SessionChannel.SessionFullListener() {
+        @Override
+        public void onSessionFull() {
+            Log.d(TAG, "onSessionFull");
+            Toast.makeText(MainActivity.this, "Session is full", Toast.LENGTH_LONG).show();
+        }
+    };
+
+    private SimpleStreamSet mStreamSet;
+    private VideoView mRemoteView;
+
+    @Bind(R.id.join)
+    Button mJoinButton;
+
+    @Bind(R.id.call)
+    Button mCallButton;
+
+    @OnClick(R.id.join)
+    public void onJoinClick(View view) {
+        Log.d(TAG, "onJoinClick");
+
+        mSessionChannel = new SessionChannel(BuildConfig.SERVER, BuildConfig.SESSION_ID);
+        mSessionChannel.setJoinListener(mJoinListener);
+        mSessionChannel.setDisconnectListener(mDisconnectListener);
+        mSessionChannel.setSessionFullListener(mSessionFullListener);
+
+        mStreamSet = SimpleStreamSet.defaultConfig(BuildConfig.USE_AUDIO, BuildConfig.USE_VIDEO);
+        mRemoteView = mStreamSet.createRemoteView();
+        mRemoteView.setView(mVideoView);
+    }
+
+    @OnClick(R.id.call)
+    public void onCallClick(View view) {
+        Log.d(TAG, "onCallClick");
+        mRtcSession.start(mStreamSet);
+    }
+
+    private RtcSession mRtcSession;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
+
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
 
-        Owr.getCaptureSources(EnumSet.of(MediaType.VIDEO), new CaptureSourcesCallback() {
-            @Override
-            public void onCaptureSourcesCallback(final List<MediaSource> mediaSources) {
-                MediaSource mediaSource = mediaSources.get(0);
-
-                VideoRenderer videoRenderer = new VideoRenderer(TAG);
-                videoRenderer.setWidth(720);
-                videoRenderer.setHeight(720);
-                videoRenderer.setSource(mediaSource);
-            }
-        });
-
-        mTextureView = (TextureView) findViewById(R.id.texture_view);
-        mTextureView.setSurfaceTextureListener(mSurfaceTextureListener);
+        mSessionId.setText(BuildConfig.SESSION_ID);
     }
+
 }
---
+++ app/src/main/java/com/vuzix/samplewebrtc/android/SessionChannel.java
@@ -0,0 +1,319 @@
+package com.vuzix.samplewebrtc.android;
+
+import android.os.Handler;
+import android.os.Looper;
+import android.util.Log;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.math.BigInteger;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.security.SecureRandom;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+public class SessionChannel {
+    public static final String TAG = SessionChannel.class.getSimpleName();
+
+    public static final int USER_ID_BITS = 40;
+    public static final int USER_ID_RADIX = 32;
+
+    private final SecureRandom mRandom = new SecureRandom();
+    private final Map<String, Peer> mPeers = new HashMap<>();
+
+    private final Handler mMainHandler = new Handler();
+    private Handler mSendHandler;
+
+    private final String mClientToServerUrl;
+    private final String mServerToClientUrl;
+
+    private JoinPeerListener mJoinListener;
+    private DisconnectListener mDisconnectListener;
+    private SessionFullListener mSessionFullListener;
+
+    public void setJoinListener(JoinPeerListener joinListener) {
+        mJoinListener = joinListener;
+    }
+
+    public void setDisconnectListener(DisconnectListener disconnectListener) {
+        mDisconnectListener = disconnectListener;
+    }
+
+    public void setSessionFullListener(SessionFullListener sessionFullListener) {
+        mSessionFullListener = sessionFullListener;
+    }
+
+    public SessionChannel(String serverUrl, String session) {
+        String userId = new BigInteger(USER_ID_BITS, mRandom).toString(USER_ID_RADIX);
+        mServerToClientUrl = serverUrl + "/stoc/" + session + "/" + userId;
+        mClientToServerUrl = serverUrl + "/ctos/" + session + "/" + userId;
+        open();
+    }
+
+    private ConnectionThread mConnectionThread;
+
+    private synchronized void open() {
+        close();
+
+        // 送信用ハンドラを作成
+        new Thread() {
+            @Override
+            public void run() {
+                Looper.prepare();
+                mSendHandler = new Handler();
+                Looper.loop();
+            }
+        }.start();
+
+        mConnectionThread = new ConnectionThread();
+        mConnectionThread.start();
+    }
+
+    private synchronized void close() {
+        if (mConnectionThread == null) {
+            return;
+        }
+
+        mConnectionThread.isFinished.set(true);
+        mConnectionThread = null;
+    }
+
+    private class ConnectionThread extends Thread {
+
+        private static final String EVENT = "event";
+        private static final String DATA = "data";
+        public static final String PREFIX_USER = "user-";
+        public static final String JOIN = "join";
+        public static final String LEAVE = "leave";
+        public static final String SESSION_FULL = "sessionfull";
+
+        private final AtomicBoolean isFinished = new AtomicBoolean();
+
+        @Override
+        public void run() {
+            Log.d(TAG, "ConnectionThread started.");
+
+            Log.d(TAG, "connecting....");
+
+            HttpURLConnection urlConnection = null;
+            try {
+                Log.d(TAG, mServerToClientUrl);
+                urlConnection = (HttpURLConnection) new URL(mServerToClientUrl).openConnection();
+
+                urlConnection.setRequestMethod("GET");
+                urlConnection.setDoInput(true);
+                urlConnection.setDoOutput(false);
+                urlConnection.connect();
+
+                BufferedReader bufferedReader = new BufferedReader(
+                        new InputStreamReader(urlConnection.getInputStream()));
+
+                readEvent(bufferedReader);
+
+            } catch (IOException e) {
+                Log.e(TAG, "IOException", e);
+            } finally {
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+                mMainHandler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        for (Peer peer : mPeers.values()) {
+                            peer.onDisconnect();
+                        }
+
+                        if (mDisconnectListener != null) {
+                            mDisconnectListener.onDisconnect();
+                        }
+                    }
+                });
+
+            }
+
+        }
+
+        private void readEvent(final BufferedReader bufferedReader) throws IOException {
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                if (line.length() > 1) {
+                    String[] eventSplit = line.split(":", 2);
+
+                    if (eventSplit.length != 2 || !eventSplit[0].equals("event")) {
+                        Log.w(TAG, "SSE: invalid event: " + line + " => " + Arrays.toString(eventSplit));
+                        while (!(line = bufferedReader.readLine()).isEmpty()) {
+                            Log.w(TAG, "SSE: skipped after malformed event: " + line);
+                        }
+                        break;
+                    }
+
+                    final String event = eventSplit[1];
+
+                    if ((line = bufferedReader.readLine()) != null) {
+                        final String[] dataSplit = line.split(":", 2);
+
+                        if (dataSplit.length != 2 || !dataSplit[0].equals("data")) {
+                            Log.w(TAG, "SSE: invalid data: " + line + " => " + Arrays.toString(dataSplit));
+                        }
+                        final String data = dataSplit[1];
+
+                        mMainHandler.post(new Runnable() {
+                            @Override
+                            public void run() {
+                                handleEvent(event, data);
+                            }
+                        });
+                    }
+                }
+            }
+        }
+
+        private void handleEvent(String event, String data) {
+            Log.d(TAG, "event:" + event + ", data:" + data);
+
+            if (event.startsWith(PREFIX_USER)) {
+                String peerId = event.substring(PREFIX_USER.length());
+                Peer peerChannel = mPeers.get(peerId);
+                if (peerChannel != null) {
+                    peerChannel.onMessage(data);
+                }
+            } else if (event.equals(JOIN)) {
+                Peer peerChannel = new Peer(mClientToServerUrl, data);
+                mPeers.put(data, peerChannel);
+                if (mJoinListener != null) {
+                    mJoinListener.onPeerJoin(peerChannel);
+                }
+            } else if (event.equals(LEAVE)) {
+                Peer peerChannel = mPeers.remove(data);
+                if (peerChannel != null) {
+                    peerChannel.onDisconnect();
+                }
+            } else if (event.equals(SESSION_FULL)) {
+                if (mSessionFullListener != null) {
+                    mSessionFullListener.onSessionFull();
+                }
+            } else {
+                Log.w(TAG, "unknown event: " + event);
+            }
+        }
+    }
+
+    public interface MessageListener {
+        void onMessage(JSONObject data);
+    }
+
+    public interface SessionFullListener {
+        void onSessionFull();
+    }
+
+    public interface DisconnectListener {
+        void onDisconnect();
+    }
+
+    public interface JoinPeerListener {
+        void onPeerJoin(Peer peer);
+    }
+
+    public interface PeerDisconnectListener {
+        void onPeerDisconnect(Peer peer);
+    }
+
+    public void send(final Peer peer, final JSONObject message) {
+        mSendHandler.post(new Runnable() {
+            @Override
+            public void run() {
+                if (peer.mDisconnected) {
+                    return;
+                }
+
+                HttpURLConnection urlConnection = null;
+                try {
+                    urlConnection = (HttpURLConnection) new URL(peer.mPeerUrl).openConnection();
+                    urlConnection.setRequestMethod("POST");
+                    urlConnection.setDoOutput(true);
+
+                    OutputStream os = urlConnection.getOutputStream();
+                    os.write(message.toString().getBytes("UTF-8"));
+                    os.flush();
+                    os.close();
+
+                    int responseCode = urlConnection.getResponseCode();
+                    if (responseCode != HttpURLConnection.HTTP_NO_CONTENT) {
+                        Log.e(TAG, "response " + responseCode);
+                    }
+
+                } catch (IOException e) {
+                    Log.e(TAG, "IOException", e);
+                } finally {
+                    if (urlConnection != null) {
+                        urlConnection.disconnect();
+                    }
+                }
+            }
+        });
+    }
+
+    public static class Peer {
+
+        private final String mId;
+        private final String mPeerUrl;
+        private boolean mDisconnected = false;
+
+        private MessageListener mMessageListener;
+        private PeerDisconnectListener mDisconnectListener;
+
+        public String getId() {
+            return mId;
+        }
+
+        private Peer(String serverUrl, String peerId) {
+            mId = peerId;
+            mPeerUrl = serverUrl + "/" + peerId;
+        }
+
+        private void onMessage(String message) {
+            Log.d(TAG, "onMessage " + message);
+
+            if (mDisconnected) {
+                return;
+            }
+            if (mMessageListener != null) {
+                try {
+                    JSONObject json = new JSONObject(message);
+                    mMessageListener.onMessage(json);
+                } catch (JSONException e) {
+                    Log.e(TAG, "JSONException", e);
+                }
+            }
+        }
+
+        private void onDisconnect() {
+            Log.d(TAG, "onDisconnect ");
+
+            mDisconnected = true;
+
+            if (mDisconnectListener != null) {
+                mDisconnectListener.onPeerDisconnect(this);
+                mDisconnectListener = null;
+                mMessageListener = null;
+            }
+        }
+
+        public void setMessageListener(MessageListener messageListener) {
+            mMessageListener = messageListener;
+        }
+
+        public void setDisconnectListener(PeerDisconnectListener onDisconnectListener) {
+            mDisconnectListener = onDisconnectListener;
+        }
+
+    }
+}
