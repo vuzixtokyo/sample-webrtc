a807495321db292b7cec7b09916870f5cfd4e79c
Keiji Ariyama <keiji_ariyama@c-lis.co.jp>
2016-02-25 17:48:17 +0900

    OpenWebRTCを組み込んだ。

--- app/src/main/java/com/vuzix/samplewebrtc/android/MainActivity.java
+++ app/src/main/java/com/vuzix/samplewebrtc/android/MainActivity.java
@@ -1,13 +1,75 @@
 package com.vuzix.samplewebrtc.android;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.pm.ActivityInfo;
+import android.graphics.SurfaceTexture;
 import android.os.Bundle;
+import android.view.Surface;
+import android.view.TextureView;
+import android.view.Window;
 
-public class MainActivity extends AppCompatActivity {
+import com.ericsson.research.owr.CaptureSourcesCallback;
+import com.ericsson.research.owr.MediaSource;
+import com.ericsson.research.owr.MediaType;
+import com.ericsson.research.owr.Owr;
+import com.ericsson.research.owr.VideoRenderer;
+import com.ericsson.research.owr.WindowRegistry;
+
+import java.util.EnumSet;
+import java.util.List;
+
+public class MainActivity extends Activity {
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    static {
+        Owr.init();
+        Owr.runInBackground();
+    }
+
+    private TextureView mTextureView;
+    private final TextureView.SurfaceTextureListener mSurfaceTextureListener
+            = new TextureView.SurfaceTextureListener() {
+        @Override
+        public void onSurfaceTextureAvailable(final SurfaceTexture surfaceTexture, final int width, final int height) {
+            Surface surface = new Surface(surfaceTexture);
+            WindowRegistry.get().register(TAG, surface);
+        }
+
+        @Override
+        public void onSurfaceTextureSizeChanged(final SurfaceTexture surface, final int width, final int height) {
+        }
+
+        @Override
+        public boolean onSurfaceTextureDestroyed(final SurfaceTexture surface) {
+            WindowRegistry.get().unregister(TAG);
+            return true;
+        }
+
+        @Override
+        public void onSurfaceTextureUpdated(final SurfaceTexture surface) {
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
+
+        Owr.getCaptureSources(EnumSet.of(MediaType.VIDEO), new CaptureSourcesCallback() {
+            @Override
+            public void onCaptureSourcesCallback(final List<MediaSource> mediaSources) {
+                MediaSource mediaSource = mediaSources.get(0);
+
+                VideoRenderer videoRenderer = new VideoRenderer(TAG);
+                videoRenderer.setWidth(720);
+                videoRenderer.setHeight(720);
+                videoRenderer.setSource(mediaSource);
+            }
+        });
+
+        mTextureView = (TextureView) findViewById(R.id.texture_view);
+        mTextureView.setSurfaceTextureListener(mSurfaceTextureListener);
     }
 }
